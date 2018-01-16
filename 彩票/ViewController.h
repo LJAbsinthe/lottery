4a205aaaebf89e7a466378e1ba4a45c37140d473
liujia <liujia01@17guagua.com>
2018-01-16 11:08:18 +0800

    Initial Commit

---
+++ 彩票/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  彩票
+//
+//  Created by LJ on 2018/1/16.
+//  Copyright © 2018年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
